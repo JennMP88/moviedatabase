5eeae0f6dba9dd269d4540977303692577b7adc4
Jennifer Perez <jenniferperez@pursuit.org>
2019-04-13 16:39:37 -0400

    added read movie by id and all movies

--- routes/movies.js
+++ routes/movies.js
@@ -2,7 +2,7 @@ const express=require('express')
 const moviesRouter=express.Router();
 const MoviesServices=require('../services/movies')
 
-//----------GET ALL MOVIES
+//----------GET ALL MOVIES by ID 
 moviesRouter.get('/movies/:id', (req, res, next) => {
   const {id} = req.params;
   
@@ -16,6 +16,30 @@ moviesRouter.get('/movies/:id', (req, res, next) => {
 });
 
 
+//-------READS ALL MOVIES
+moviesRouter.get('/movies', (req, res, next) => {
+  
+  MoviesServices.readsall()
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      next(err);
+    })
+});
+
+//-----READS ALL MOVIES BY GENRE
+moviesRouter.get('/movies', (req, res, next) => {
+  const {} = req.body;
+  
+  MoviesServices.readmoviegenre()
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      next(err);
+    })
+});
 
 
 module.exports=moviesRouter;
--- services/movies.js
+++ services/movies.js
@@ -2,7 +2,7 @@ const {db} = require('./dbConnect');
 const MoviesServices = {};
 
 
-
+//---------Reads all movies by id
 MoviesServices.read= (id) => {
     const sql = `
    SELECT *
@@ -10,5 +10,21 @@ MoviesServices.read= (id) => {
    WHERE id= ${id} `
     return db.any(sql, {id});
   }
+
+  //-------Reads all movies 
+  MoviesServices.readsall= () => {
+    const sql = `
+   SELECT *
+   FROM movies`
+    return db.any(sql, {});
+  }
+
+  //--------Reads all movies by genre
+  // MoviesServices.readmoviegenre= () => {
+  //   const sql = `
+  //  SELECT *
+  //  FROM movies`
+  //   return db.any(sql, {});
+  // }
 
 module.exports = MoviesServices;
